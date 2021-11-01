5dab86ba8334be814a677d037eda8e258f07093d
Vadim Machikhin <V.machikhin@bk.ru>
2021-11-01 23:25:48 +0500

    Second commit

--- src/Sum.java
+++ src/Sum.java
@@ -6,6 +6,8 @@ class Sum {
         System.out.println("Введите 2 любых дробных числа: ");
         double number1 = scan.nextDouble();
         double number2 = scan.nextDouble();
-        System.out.print("ответ " + (number1 + number2));
+        double number3 = scan.nextDouble();
+        double number4 = scan.nextDouble();
+        System.out.print("ответ " + (number1 + number2 + number3 + number4));
     }
 }
